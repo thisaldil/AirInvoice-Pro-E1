b9b6a9fd0b27497e473f5a5d6089f52ae2cefee0
sandeepa <sandeepamallawarachchi444@gmail.com>
2025-04-02 16:04:15 +0530

    feat: implement invoice management with save and retrieve functionality in userController

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 router.get('/getUserDetails/:userId', userController.getUserDetails);
 router.post('/sendInvoiceEmail', userController.sendInvoiceEmail);
+router.post('/saveInvoiceDetails', userController.saveInvoiceDetails);
+router.get('/getInvoiceDetailsByUserId/:userId', userController.getInvoiceDetailsByUserId);
 
 module.exports = router;
