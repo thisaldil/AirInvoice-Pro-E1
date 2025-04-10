7de52cb88f8dc5d18e6b6eaf51a1025c10838bdf
sandeepa <sandeepamallawarachchi444@gmail.com>
2025-04-10 11:38:44 +0530

    feat: implement template fetching, updating, and deletion in TemplateManager and templateController

--- server/controllers/templateController.js
+++ server/controllers/templateController.js
@@ -4,6 +4,7 @@ const nodemailer = require("nodemailer");
 const Invoice = require("../models/Invoice");
 const Template = require("../models/Template.js");
 
+//save template
 exports.createTemplate = async (req, res) => {
     try {
         const newTemplate = new Template(req.body);
@@ -13,3 +14,54 @@ exports.createTemplate = async (req, res) => {
         res.status(500).json({ error: "Failed to save template" });
     }
 };
+
+//get all templates
+exports.getTemplates = async (req, res) => {
+    try {
+        const templates = await Template.find({});
+        res.status(200).json(templates);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch templates" });
+    }
+};
+
+//get template by id
+exports.getTemplateById = async (req, res) => {
+    try {
+        const template = await Template.findById(req.params.id);
+        if (!template) {
+            return res.status(404).json({ error: "Template not found" });
+        }
+        res.status(200).json(template);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch template" });
+    }
+};
+
+//update template by id
+exports.updateTemplate = async (req, res) => {
+    try {
+        const template = await Template.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        });
+        if (!template) {
+            return res.status(404).json({ error: "Template not found" });
+        }
+        res.status(200).json(template);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to update template" });
+    }
+}
+
+//delete template by id
+exports.deleteTemplate = async (req, res) => {
+    try {
+        const template = await Template.findByIdAndDelete(req.params.id);
+        if (!template) {
+            return res.status(404).json({ error: "Template not found" });
+        }
+        res.status(200).json({ message: "Template deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ error: "Failed to delete template" });
+    }
+}
--- server/routes/templateRoutes.js
+++ server/routes/templateRoutes.js
@@ -4,5 +4,9 @@ const templateController = require("../controllers/templateController.js");
 
 
 router.post("/createTemplate", templateController.createTemplate);
+router.get("/getTemplates", templateController.getTemplates);
+router.get("/getTemplateById/:id", templateController.getTemplateById);
+router.put("/updateTemplate/:id", templateController.updateTemplate);
+router.delete("/deleteTemplate/:id", templateController.deleteTemplate);
 
 module.exports = router;
