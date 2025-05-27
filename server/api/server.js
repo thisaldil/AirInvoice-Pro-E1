1b02267306f20c27a6c9bd7269e3a007c8d60f5e
sandeepa <126542051+sandeepaMallawarachchi@users.noreply.github.com>
2025-05-27 18:04:55 +0530

    Implement server setup and routing for Vercel deployment

--- server/api/server.js
+++ server/api/server.js
@@ -65,5 +65,8 @@ app.post("/generate-signature", (req, res) => {
 connectDB();
 
 // Start server
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => console.log(`Server running on port ${port}`));
+
+//export app for vercel
+module.exports = app;
