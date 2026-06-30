5fced0647d5a1350a5c502f15f8e2fce7a27b60e
Dimuthu Abmsz <dimuthu.abmsz.work@gmail.com>
2026-06-30 08:57:22 +0530

    fix:modify user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -19,7 +19,10 @@ const userSchema = new Schema(
     password: { type: String, required: true},
     verifyotp: { type: Number, default:" "},
     verifyotpExpireat: { type: Number, default:0},
-  },
+isAccountVerified: { type: Boolean, default: false },
+resetOtp: { type: String, default: "" },
+resetOtpExpireAt: { type: Number, default: 0 },  
+},
   { timestamps: true }
 );
 
